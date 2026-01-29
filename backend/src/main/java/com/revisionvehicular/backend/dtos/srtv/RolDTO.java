7079839cbd69ec4f990df63e140aa09061a38a91
yaser-uwu <destructor12342@gmail.com>
2026-01-29 16:53:41 -0500

    ROL YA LISTO

---
+++ backend/src/main/java/com/revisionvehicular/backend/dtos/srtv/RolDTO.java
@@ -0,0 +1,9 @@
+package com.revisionvehicular.backend.dtos.srtv;
+import lombok.Data;
+
+@Data
+public class RolDTO {
+    private Long rolId;
+    private String nombre;
+    private String estado;
+}
