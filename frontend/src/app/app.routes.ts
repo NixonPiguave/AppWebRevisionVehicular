c3d94d332f74d7d131d064a3aa0042604faa1f17
NixonPiguave <NixonPiguave>
2026-01-30 16:20:54 -0500

    Lo de administracion en interfaz listo, falta las interfaces de CRUD en eso ando

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,22 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login';
 import { InicioComponent } from './paginas/inicio/inicio';
-import {AreasComponent} from './paginas/administracion/areas/areas';
-import {EmpresaComponent} from './paginas/administracion/empresa/empresa';
-import {RolesComponent} from './paginas/administracion/roles/roles';
-import {UsuariosComponent} from './paginas/administracion/usuarios/usuarios';
+import { AreasComponent } from './paginas/administracion/areas/areas';
+import { EmpresaComponent } from './paginas/administracion/empresa/empresa';
+import { RolesComponent } from './paginas/administracion/roles/roles';
+import { UsuariosComponent } from './paginas/administracion/usuarios/usuarios';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'inicio', component: InicioComponent },
+  {
+    path: 'inicio',
+    component: InicioComponent,
+    children: [
+      { path: 'administracion/usuarios', component: UsuariosComponent },
+      { path: 'administracion/roles', component: RolesComponent },
+      { path: 'administracion/areas', component: AreasComponent },
+      { path: 'administracion/empresa', component: EmpresaComponent },
+    ]
+  },
   { path: '**', redirectTo: '' }
 ];
